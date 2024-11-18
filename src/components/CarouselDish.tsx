7b2d3318efa1573ed9406066fa13d149db20e195
luis pina <luispia1290@gmail.com>
2024-11-17 21:33:26 -0600

    Se agrego condicion para mostrar diferentes carouseles

--- src/components/CarouselDish.tsx
+++ src/components/CarouselDish.tsx
@@ -1,11 +1,15 @@
 import Slider from "react-slick";
 import { ItemCarousel, PropsItems } from "./ItemCarousel";
+import { useLocation } from "react-router-dom";
+import { DetailTemplate } from "../templates/DetailTemplate";
+import { CardRecomendaciones } from "./CardRecomendaciones";
 
 interface PropsCarousel {
   arrayItems: PropsItems[];
 }
 
 export const CarouselDish = ({ arrayItems }: PropsCarousel) => {
+  const { pathname } = useLocation();
   const settings = {
     dots: false,
     infinite: true,
@@ -45,18 +49,27 @@ export const CarouselDish = ({ arrayItems }: PropsCarousel) => {
 
   return (
     <div className="slider-container">
-      <Slider {...settings}>
-        {arrayItems.map((item, index) => (
-          <ItemCarousel
-            key={index}
-            idPlatillo={item.idPlatillo}
-            titulo={item.titulo}
-            subtitulo={item.subtitulo}
-            img={item.img}
-            precio={item.precio}
-          />
-        ))}
-      </Slider>
+      {
+        pathname === "/menu/main" ? (
+          <Slider {...settings}>
+            <CardRecomendaciones />
+            <CardRecomendaciones />
+          </Slider>
+        ) : (
+          <Slider {...settings}>
+            {arrayItems.map((item, index) => (
+              <ItemCarousel
+                key={index}
+                idPlatillo={item.idPlatillo}
+                titulo={item.titulo}
+                subtitulo={item.subtitulo}
+                img={item.img}
+                precio={item.precio}
+              />
+            ))}
+          </Slider>
+        )
+      }
     </div>
   );
 };
